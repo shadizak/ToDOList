eb91ad97263cfbf8e9a00f530bfeca2871c8e8c5
shadizak <shadizakout@hotmail.com>
2018-05-20 16:46:02 +0300

    Core Data

---
+++ ToDoList-FinalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoList-FinalProject
+//
+//  Created by shadi on 5/20/18.
+//  Copyright © 2018 shadi Zaqout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
